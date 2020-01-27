a63be63810a588191d43ed040ee1a7db872332ff
Kazuhito Ochiai <kochiai@grubhub.com>
2020-01-27 12:53:09 -0500

    Initial Commit

---
+++ CurrentOrderFooter_POC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrentOrderFooter_POC
+//
+//  Created by Kazuhito Ochiai on 1/27/20.
+//  Copyright © 2020 Grubhub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
